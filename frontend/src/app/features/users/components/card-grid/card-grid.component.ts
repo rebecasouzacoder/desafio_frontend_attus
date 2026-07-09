ad62b01145ef185e5b5ccb02c520e3c540687343
Seu <rebeca.rnsouza@gmail.com>
2026-07-09 19:28:26 -0300

    docs(users): add component documentation

--- frontend/src/app/features/users/components/card-grid/card-grid.component.ts
+++ frontend/src/app/features/users/components/card-grid/card-grid.component.ts
@@ -1,16 +1,31 @@
 import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { IUser } from '../../models/user.model';
 
+/**
+ * Componente responsável pela apresentação visual
+ * da lista de usuários em formato de cards.
+ *
+ * @export
+ * @class CardGridComponent
+ */
 @Component({
   selector: 'app-card-grid',
   imports: [MatCardModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './card-grid.component.html',
-  styleUrl: './card-grid.component.scss'
+  styleUrl: './card-grid.component.scss',
 })
 export class CardGridComponent {
-public usersList = input.required<IUser[]>();
+  /**
+   *  Lista de usuários que serão exibidos nos cards.
+   * 
+   * O valor é obrigatório e deve ser informado pelo
+   * componente pai.
+   *
+   * @memberof CardGridComponent
+   */
+  public usersList = input.required<IUser[]>();
 }
